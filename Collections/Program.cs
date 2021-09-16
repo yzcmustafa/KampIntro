0c075b4e64e169ae6931153fcbf6d9c148c214fc
yzcmustafa <mustafayazici@ogr.bandirma.edu.tr>
2021-09-16 13:13:53 +0300

    Çalışma

---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "engin", "murat", "hasan", "murat" };
+
+            List<string> isimler2 = new List<string> { "engin", "murat", "hasan", "murat" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
